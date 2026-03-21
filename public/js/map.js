cc2f8eec5c9847672e1828d300082cd3d0652e30
Manoj <manojkunwar626@gmail.com>
2026-03-21 15:50:14 +0530

    Updated category bar UI

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,35 @@
-//  mapboxgl.accessToken = mapToken;
+  mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+   style: 'mapbox://styles/mapbox/streets-v12',
+    center: listing.geometry.coordinates,
+    zoom: 9
+});
+
+function changeStyle(style){
+    const styles = {
+        streets: "mapbox://styles/mapbox/streets-v12",
+        dark: "mapbox://styles/mapbox/dark-v11",
+        light: "mapbox://styles/mapbox/light-v11",
+        satellite: "mapbox://styles/mapbox/satellite-v9"
+    };
+
+    map.setStyle(styles[style]);
+}
+const marker = new mapboxgl.Marker({ color: 'red' })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({offset: 25})
+   .setHTML(
+        `<h4>${listing.title}</h4>
+         <p>${listing.location}</p>
+         <p>Exact Location will be provided after booking!</p>`
+      )
+  )
+  .addTo(map);
+
+// mapboxgl.accessToken = mapToken;
+
 // const map = new mapboxgl.Map({
 //     container: 'map',
 //     style: 'mapbox://styles/mapbox/streets-v12',
@@ -16,47 +47,16 @@
 
 //     map.setStyle(styles[style]);
 // }
-// const marker = new mapboxgl.Marker({ color: 'red' })
-//   .setLngLat(listing.geometry.coordinates)
-//   .setPopup(
-//     new mapboxgl.Popup({offset: 25})
-//    .setHTML(
-//         `<h4>${listing.title}</h4>
-//          <p>${listing.location}</p>
-//          <p>Exact Location will be provided after booking!</p>`
-//       )
-//   )
-//   .addTo(map);
-
-mapboxgl.accessToken = mapToken;
-
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: listing.geometry.coordinates,
-    zoom: 9
-});
-
-function changeStyle(style){
-    const styles = {
-        streets: "mapbox://styles/mapbox/streets-v12",
-        dark: "mapbox://styles/mapbox/dark-v11",
-        light: "mapbox://styles/mapbox/light-v11",
-        satellite: "mapbox://styles/mapbox/satellite-v9"
-    };
-
-    map.setStyle(styles[style]);
-}
 
 // Popup
-const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-    `<h4>${listing.title}</h4>
-     <p>${listing.location}</p>
-     <p>Exact Location will be provided after booking!</p>`
-);
+// const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+//     `<h4>${listing.title}</h4>
+//      <p>${listing.location}</p>
+//      <p>Exact Location will be provided after booking!</p>`
+// );
 
-// Marker
-const marker = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(listing.geometry.coordinates)
-  .setPopup(popup)  
-  .addTo(map);
+// // Marker
+// const marker = new mapboxgl.Marker({ color: "red" })
+//   .setLngLat(listing.geometry.coordinates)
+//   .setPopup(popup)  
+//   .addTo(map);

a83e58f71e353854b27a0fb315b6f765b8334906
Manoj <manojkunwar626@gmail.com>
2026-03-21 15:58:26 +0530

    Fix homepage route

--- app.js
+++ app.js
@@ -112,7 +112,6 @@
 // app.listen(8080, () => {
 //     console.log("server is listening on port 8080");
 // });
-
 if (process.env.NODE_ENV != "production") {
    require('dotenv').config(); 
 }
@@ -131,96 +130,105 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+// Routes
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const pagesRouter = require("./routes/pages.js");   
+const pagesRouter = require("./routes/pages.js");
 
+// DB URL
+const dbUrl = process.env.ATLASDB_URL;
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
- const dbUrl = process.env.ATLASDB_URL;
-
+// Connect DB
 main()
-    .then(() => { console.log("connected to DB"); })
-    .catch((err) => { console.log(err); });
+  .then(() => console.log("connected to DB"))
+  .catch(err => console.log(err));
 
 async function main() {
-    // await mongoose.connect(MONGO_URL);
-    await mongoose.connect(dbUrl);
-
+  await mongoose.connect(dbUrl);
 }
 
+// View engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
 
-
-
-
+// Session store
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SESSION_SECRET,
-    },
-    touchAfter: 24 * 3600,
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SESSION_SECRET,
+  },
+  touchAfter: 24 * 3600,
 });
 
 store.on("error", (err) => {
-    console.log("ERROR in MONGO SESSION STORE", err);
+  console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
-    store,
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true
-    },
+  store,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
-
-
 
 app.use(session(sessionOptions));
 app.use(flash());
 
+// Passport config
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Flash + user middleware
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
 
+
+//  Homepage route 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
+
+// Routes
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
-app.use("/", pagesRouter);   
+app.use("/", pagesRouter);
 
 
+// 404 handler
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found!"));
+  next(new ExpressError(404, "Page Not Found!"));
 });
 
+// Error handler
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong!" } = err;
-    res.status(statusCode).render("error.ejs", { message });
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
 });
 
+// Server
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });

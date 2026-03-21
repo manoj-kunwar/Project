4e4fa052a5f5d329044a1c98c031b987685c6e62
Manoj <manojkunwar626@gmail.com>
2026-03-21 14:52:36 +0530

    Fixed port for deployment

--- app.js
+++ app.js
@@ -219,6 +219,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-    console.log("server is listening on port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });

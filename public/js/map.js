81c6f0c19ca2d9b9b951ec51953c7b5a778152eb
Manoj <manojkunwar626@gmail.com>
2026-05-25 08:00:19 +0530

    Improved comments

--- public/js/map.js
+++ public/js/map.js
@@ -27,36 +27,3 @@ const marker = new mapboxgl.Marker({ color: 'red' })
       )
   )
   .addTo(map);
-
-// mapboxgl.accessToken = mapToken;
-
-// const map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v12',
-//     center: listing.geometry.coordinates,
-//     zoom: 9
-// });
-
-// function changeStyle(style){
-//     const styles = {
-//         streets: "mapbox://styles/mapbox/streets-v12",
-//         dark: "mapbox://styles/mapbox/dark-v11",
-//         light: "mapbox://styles/mapbox/light-v11",
-//         satellite: "mapbox://styles/mapbox/satellite-v9"
-//     };
-
-//     map.setStyle(styles[style]);
-// }
-
-// Popup
-// const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-//     `<h4>${listing.title}</h4>
-//      <p>${listing.location}</p>
-//      <p>Exact Location will be provided after booking!</p>`
-// );
-
-// // Marker
-// const marker = new mapboxgl.Marker({ color: "red" })
-//   .setLngLat(listing.geometry.coordinates)
-//   .setPopup(popup)  
-//   .addTo(map);
--- routes/pages.js
+++ routes/pages.js
@@ -1,21 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// // Help Centre
-// router.get("/help",          (req, res) => res.render("pages/help"));
-// router.get("/help/home",     (req, res) => res.render("pages/help-home"));
-// router.get("/help/booking",  (req, res) => res.render("pages/help-booking"));
-// router.get("/help/payments", (req, res) => res.render("pages/help-payments"));
-// router.get("/help/safety",   (req, res) => res.render("pages/help-safety"));
-// router.get("/help/reviews",  (req, res) => res.render("pages/help-reviews"));
-// router.get("/help/account",  (req, res) => res.render("pages/help-account"));
-
-// // Other pages
-// router.get("/refer-host",    (req, res) => res.render("pages/refer-host"));
-// router.get("/co-host",       (req, res) => res.render("pages/co-host"));
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 

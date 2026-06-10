3d8ba70dfc351c98e476b9cdf97ceec094f58717
Manoj <manojkunwar626@gmail.com>
2026-06-10 10:42:44 +0530

    improve the mapbox

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use((req, res, next) => {
   res.locals.success  = req.flash("success");
   res.locals.error    = req.flash("error");
   res.locals.currUser = req.user;
-  res.locals.search   = req.query.search || '';  // ← FIX
+  res.locals.search   = req.query.search || '';  
   next();
 });
 

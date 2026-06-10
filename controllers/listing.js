2d753a4f960fa519d7c1ae256122335c9ac5c5ec
Manoj <manojkunwar626@gmail.com>
2026-06-10 11:07:06 +0530

    Fix Mapbox token passing

--- controllers/listing.js
+++ controllers/listing.js
@@ -33,20 +33,40 @@ module.exports.renderNewForm = (req, res) => {
     res.render("listings/new.ejs");
 };
 
+// module.exports.showListing = async (req, res) => {
+//     let { id } = req.params;
+//     const listing = await Listing.findById(id)
+//         .populate({
+//             path: "reviews",
+//             populate: { path: "author" },
+//         })
+//         .populate("owner");
+//     if (!listing) {
+//         req.flash("error", "Listing you requested for does not exist!");
+//         return res.redirect("/listings");
+//     }
+//     console.log(listing);
+//     res.render("listings/show", { listing });
+// };
 module.exports.showListing = async (req, res) => {
     let { id } = req.params;
+
     const listing = await Listing.findById(id)
         .populate({
             path: "reviews",
             populate: { path: "author" },
         })
         .populate("owner");
+
     if (!listing) {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
     }
-    console.log(listing);
-    res.render("listings/show", { listing });
+
+    res.render("listings/show", {
+        listing,
+        mapToken: process.env.MAP_TOKEN
+    });
 };
 
 module.exports.createListing = async (req, res, next) => {
